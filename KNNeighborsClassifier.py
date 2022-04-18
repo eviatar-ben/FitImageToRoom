77a538e4bb7091a1eb6dde91f91e2cbd535525ad
eviatar <eviatar>
2022-04-19 00:49:11 +0300

    knnclasifier is working on train data

---
+++ KNNeighborsClassifier.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+
+INPUT_PATH = r'C:\Users\eviatar\OneDrive\Documents\Python Venv\FitImage\vector_to_predict'
+DATA_PATH = r'C:\Users\eviatar\OneDrive\Documents\Python Venv\FitImage\TrainData'
+
+
+def split_data(df):
+    # y = df['Image_name'].to_numpy()
+    # df = df.iloc[:, 1:]
+    # # df.insert(0, 'intercept', 1)
+    # x = df.to_numpy()
+    y = df.iloc[:, 0]
+    x = df.iloc[:, 1:]
+    return x.to_numpy(), y.to_numpy()
+
+
+def main():
+    df = pd.read_csv(DATA_PATH)
+    x, y = split_data(df)
+    classifier = KNeighborsClassifier(n_neighbors=3)
+    classifier.fit(x, y)
+    v = pd.read_csv(INPUT_PATH, squeeze=True).iloc[:, 1:].to_numpy()
+
+    # todo: check why transpose were needed
+    prediction = classifier.predict(v.T)
+    print(prediction)
+
+
+if __name__ == '__main__':
+    main()
